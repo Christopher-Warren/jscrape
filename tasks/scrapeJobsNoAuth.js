0921b2b9db1d2d776af3b30f60e786ea055b82f8
Christopher Warren <chrisalmith@gmail.com>
2023-03-07 21:17:23 -0500

    add testing function

---
+++ tasks/scrapeJobsNoAuth.js
@@ -0,0 +1,173 @@
+import sendEmail from "../utils/sendEmail.js";
+import { config } from "../config.js";
+import { delay } from "../utils/delay.js";
+
+import Storage from "node-storage";
+
+import chalk from "chalk";
+import { setLoadingMessage } from "../utils/setLoadingMessage.js";
+
+export async function scrapeJobsNoAuth(page) {
+  // Is flipped after first run
+  let isFirstSearch = true;
+
+  const excludedJobs = [];
+
+  const jobsCount = 25; // 25 jobs per page
+
+  // console.log(
+  //   `Beginning search for ${chalk.blue(config.searchKeywords)} jobs.`
+  // );
+
+  // console.log(
+  //   `Excluding all jobs that contain ${chalk.yellow(
+  //     config.excludeFilter.map((i) => " " + i)
+  //   )}`
+  // );
+  // console.log(
+  //   `Including jobs that contain ${chalk.blue(
+  //     config.includeFilter.map((i) => " " + i)
+  //   )}`
+  // );
+
+  try {
+    // await page.goto(config.mainURL);
+    await page.goto(
+      "https://www.linkedin.com/jobs/search?keywords=chicken%20butcher&location=United%20States&geoId=103644278&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0"
+    );
+
+    await page.waitForSelector(".show-more-less-html__markup");
+  } catch (error) {
+    console.log(error);
+  }
+
+  // Run infinitely
+  for (;;) {
+    let start = 0;
+    const jobs = [];
+
+    // No delay for first search after starting server
+    if (!isFirstSearch) {
+      await delay(60000 * config.searchInterval);
+    } else {
+      isFirstSearch = false;
+    }
+
+    // console.log(chalk.blue("Searching for jobs..."));
+    // const clearLoadingMessage = setLoadingMessage(
+    //   "Searching for jobs...",
+    //   chalk.blue
+    // );
+
+    for (let i = 1; i <= jobsCount; i++) {
+      // As long as "No matching jobs found." is not present continue running.
+      try {
+        await page.waitForSelector(".jobs-search-no-results-banner", {
+          timeout: 3000,
+          hidden: true,
+        });
+      } catch (error) {
+        const message = await sendNewJobs({ jobs, excludedJobs });
+        console.log(message);
+
+        break;
+      }
+
+      try {
+        await updateJobs(page, i, { jobs, excludedJobs });
+      } catch (error) {
+        const message = await sendNewJobs({ jobs, excludedJobs });
+
+        console.log(message);
+
+        break;
+      }
+
+      // Go to next page when at end
+      if (i === jobsCount) {
+        i = 1;
+        start += 25; // 25 job results per page
+        await page.goto(`${config.mainURL}&start=${start}`);
+      }
+    }
+  }
+}
+
+async function updateJobs(page, i, { jobs, excludedJobs }) {
+  const store = new Storage("./store.json");
+
+  const jobContainer = await page.waitForSelector(
+    `ul:nth-child(1) li:nth-child(${i})`,
+    { timeout: 5000 }
+  );
+
+  // Scroll to job listing
+  await jobContainer.evaluate((el) => el.scrollIntoView());
+
+  // Wait for job listing link to load
+  const jobListing = await page.waitForSelector(
+    `ul:nth-child(1) li:nth-child(${i}) a`,
+    { timeout: 5000 }
+  );
+
+  // The job element
+  const val = await jobListing.evaluate((el) => {
+    // Parse link for readablility
+    const arrOfUrl = el.href.split("/");
+    // Get id
+    const jobId = arrOfUrl[5];
+
+    // remove params
+    arrOfUrl.pop();
+    const url = arrOfUrl.join("/");
+
+    return { title: el.innerText, href: url, id: el.innerText };
+  });
+
+  const bodyEl = await page.waitForSelector(".show-more-less-html__markup");
+
+  // Click and add delay to wait for body to load
+  await jobListing.click();
+  await delay(500);
+
+  // Error: Execution context was destroyed, most likely because of a navigation.
+  const body = await bodyEl.evaluate((el) => {
+    return { body: el.innerHTML };
+  });
+
+  // Check if job includes ONLY terms we want to see
+  const matchesIncludeFilter = config.includeFilter.some((cond) =>
+    val.title.toLowerCase().includes(cond.toLowerCase())
+  );
+
+  // Check if job contains any terms we DONT want to see
+  const matchesExcludeFilter = config.excludeFilter.some((cond) =>
+    val.title.toLowerCase().includes(cond.toLowerCase())
+  );
+  // Check if job already exists or has been sent already
+  const jobSent = store.get("jobs").some((i) => i.id === val.id);
+
+  if (!jobSent && matchesIncludeFilter && !matchesExcludeFilter) {
+    jobs.push(val);
+  } else {
+    excludedJobs.push(val);
+  }
+}
+
+async function sendNewJobs({ jobs, excludedJobs }) {
+  const store = new Storage("./store.json");
+  if (jobs.length > 0) {
+    const emailMessage = await sendEmail(jobs, excludedJobs.length);
+
+    const previousJobs = store.get("jobs");
+    store.put("jobs", [...previousJobs, ...jobs]);
+
+    return `${emailMessage} Waiting ${chalk.yellow(
+      config.searchInterval
+    )} minutes until next search.`;
+  } else {
+    return `No new jobs found. Waiting ${chalk.yellow(
+      config.searchInterval
+    )} minutes until next search.`;
+  }
+}
